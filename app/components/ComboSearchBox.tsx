a9472bbe871d8b8a31b3f5a7f5590dcd492fa013
Shefat-Shohan <shohan.ks3@gmail.com>
2025-02-12 23:48:14 +0600

    fix select

--- app/components/ComboSearchBox.tsx
+++ app/components/ComboSearchBox.tsx
@@ -1,25 +1,14 @@
 "use client";
-
 import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useBookContext } from "../context/BookContext";
-
 const genres = [
   {
     value: "fiction",
@@ -51,48 +40,17 @@ export function ComboSearchbox() {
   const [open, setOpen] = React.useState(false);
   const { value, setValue } = useBookContext();
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between lg:text-[14px] text-[12px]"
-        >
-          {value
-            ? genres.find((genres) => genres.value === value)?.label
-            : "Search by genre..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search by genre..." />
-          <CommandList>
-            <CommandEmpty>No genre found.</CommandEmpty>
-            <CommandGroup>
-              {genres.map((genres) => (
-                <CommandItem
-                  key={genres.value}
-                  value={genres.value}
-                  onSelect={(currentValue: string) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === genres.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {genres.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <Select value={value} onValueChange={setValue}>
+      <SelectTrigger className="w-[200px]">
+        <SelectValue placeholder="Search by genre..." />
+      </SelectTrigger>
+      <SelectContent>
+        {genres.map((genre) => (
+          <SelectItem key={genre.value} value={genre.value}>
+            {genre.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 }
